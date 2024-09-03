96450467565686f849e25eab9287e3890f38cdfb
pkyurosh <pkyurosh@gmail.com>
2024-09-03 21:31:28 +0545

    First Commit

---
+++ src/test/java/com/kyurosh/Java/Basic/JavaBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyurosh.Java.Basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ untitled/src/Main.java
@@ -0,0 +1,18 @@
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+     int temp = 15;
+     if(temp > 30){
+         System.out.println("Its Sunny Today");
+     }
+     else if (temp > 20)
+         System.out.println("Very Hot");
+
+     else
+         System.out.println("Not Sunny");
+
+    }
+}
